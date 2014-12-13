4370c5f3e0ce2b64648e4724c1d20c2e9e14d269
Sid Wiesner <sid.wiesner@gmail.com>
2014-12-13 10:42:39 -0500

    Remove old contracts project. Use lecentre files instead.

--- contracts/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'aisha');
-define('DB_PASSWORD', 'demo4you');
-define('DB_DATABASE', 'CONTRATDB');
-$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
-?>
