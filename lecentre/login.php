7f02b872781c007c95fa1380067612ba8701de78
Sid Wiesner <sid.wiesner@gmail.com>
2014-12-13 11:45:11 -0500

    Clean up login page.
    - Add FIXME to note this isn't a secure implementation.
    - Handle already logged in users by redirecting to welcome page.
    - Get rid of "remember me" checkbox which did nothing.

--- lecentre/login.php
+++ lecentre/login.php
@@ -1,6 +1,50 @@
+<?php
+// Copyright 2014 myAgro. All Rights Reserved.
+//
+// Description:
+// - Checks for login credentials. If none found, show login form. Otherwise redirect.
+// - Also handle form POST from login form, and if valid, redirect.
+include ("config.php");
+session_start();
+
+// FIXME - this is *not* a secure login implementation. This needs to be
+// revisited and re-implemented.
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Process login form
+    $myusername = strtolower(mysqli_real_escape_string($db, $_POST['username']));
+    $mypassword = mysqli_real_escape_string($db, $_POST['password']);
+
+    $sql    = "SELECT id FROM users WHERE username='$myusername' and password='$mypassword'";
+    $result = mysqli_query($db, $sql);
+    $row    = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $active = $row['id'];
+    $count  = mysqli_num_rows($result);
+
+    // If result matched $myusername and $mypassword, table row must be 1 row
+    if ($count == 1) {
+        $_SESSION['login_user'] = $myusername;
+        $_SESSION['logged']     = 'YES';
+        header("Location: welcome.php");
+    } else {
+        $error = "Your Login Name or Password is invalid";
+    }
+} else {
+    // Normal GET, so check if already logged in
+    if (isset($_SESSION['login_user'])) {
+        $user_check = $_SESSION['login_user'];
+        $ses_sql = mysqli_query($db, "select username from users where username='$user_check' ");
+        $row = mysqli_fetch_array($ses_sql, MYSQLI_ASSOC);
+        $login_session = $row['username'];
+        if (isset($login_session)) {
+            header("Location: welcome.php");
+        }
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,55 +54,25 @@
 
     <title>Ngasene Contract System</title>
 
-    <!-- Bootstrap core CSS -->
     <link href="_bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
     <link href="_css/signin.css" rel="stylesheet">
+</head>
 
-  </head>
-   <body>
-<?php
-include ("config.php");
-session_start();
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	// username and password sent from Form
-	$myusername = strtolower(mysqli_real_escape_string($db, $_POST['username']));
-	$mypassword = mysqli_real_escape_string($db, $_POST['password']);
-
-	$sql    = "SELECT id FROM users WHERE username='$myusername' and password='$mypassword'";
-	$result = mysqli_query($db, $sql);
-	$row    = mysqli_fetch_array($result, MYSQLI_ASSOC);
-	$active = $row['id'];
-	$count  = mysqli_num_rows($result);
-
-	// If result matched $myusername and $mypassword, table row must be 1 row
-	if ($count == 1) {
-		//session_register("myusername");
-		$_SESSION['login_user'] = $myusername;
-		$_SESSION['logged']     = 'YES';
-		header("location: welcome.php");
-	} else {
-		$error = "Your Login Name or Password is invalid";
-	}
-}
-?>
+<body>
 
     <div class="container">
-
-      <form class="form-signin" role="form" action="" method="post">
-        <img src="images/myagrogreennotext.png" style="width: 190px; height: 184px;">
-        <h2 class="form-signin-heading">Please sign in</h2>
-        <input type="email" name='username' class="form-control" placeholder="Email address" required autofocus>
-        <input type="password" name='password' class="form-control" placeholder="Password" required>
-        <label class="checkbox">
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-      </form>
-
+        <form class="form-signin" role="form" action="" method="post">
+            <img src="images/myagrogreennotext.png" style="width: 190px; height: 184px;">
+            <h2 class="form-signin-heading">Please sign in</h2>
+            <input type="email" name='username' class="form-control" placeholder="Email address" required autofocus>
+            <input type="password" name='password' class="form-control" placeholder="Password" required>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        </form>
     </div> <!-- /container -->
-   <script src="_js/jquery-2.1.1.min.js"></script>
-    <script src="_bootstrap/js/bootstrap.min.js"></script>
-  </body>
+
+<script src="_js/jquery-2.1.1.min.js"></script>
+<script src="_bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+
 </html>
